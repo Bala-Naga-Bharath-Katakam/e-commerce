809b5c460cd82f3391e277110725227af43fd4d7
Bala-Naga-Bharath-Katakam <109468378+Bala-Naga-Bharath-Katakam@users.noreply.github.com>
2024-10-28 00:36:59 +0530

    Gateway server activated

--- api-gateway/src/main/java/com/ecommerce/api/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/ecommerce/api/gateway/ApiGatewayApplication.java
@@ -3,11 +3,12 @@ package com.ecommerce.api.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
